157ff4a29dc56ad87ac13abc7a6f5dfed94c7723
Eileen <C16339313@mydit.ie>
2018-02-14 16:26:08 +0000

    package

---
+++ ie/dit/Tune.java
@@ -0,0 +1,33 @@
+package ie.dit;
+
+public class Tune
+{
+	private int x(public )
+	{
+
+	}
+	private String tile(public)
+	{
+
+	}
+	private String altTitle(public)
+	{
+
+	}
+	private String notation(public)
+	{
+
+	}
+
+	public String toString()
+	{
+		StringBuffer sb= new StringBuffer();
+		for(int x : x ||String title:title || String altTitle:altTitle)
+		{
+			sb.append(x,tile,altTitle);
+		}
+		return sb.toString();
+	}
+
+
+}
